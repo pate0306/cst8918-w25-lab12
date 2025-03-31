b747b58fcf460dafd461b3433b5aff40e20d6b57
pate0306 <root@Payarth>
2025-03-31 19:26:41 -0400

    update main file

---
+++ infra/tf-backend/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "backend_rg" {
+  name     = "veka0011-githubactions-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "backend_sa" {
+  name                     = "veka0011githubactions"
+  resource_group_name      = azurerm_resource_group.backend_rg.name
+  location                 = azurerm_resource_group.backend_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.backend_sa.name
+  container_access_type = "private"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.backend_rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.backend_sa.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.tfstate.name
+}
+
+output "arm_access_key" {
+  value     = azurerm_storage_account.backend_sa.primary_access_key
+  sensitive = true
+}
